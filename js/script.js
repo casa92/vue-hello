4d6bffae5c856abe5ded8d6e44a88aace530f7d0
casa92 <casartelli92@gmail.com>
2021-12-03 00:19:05 +0100

    fatto

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+Vue.config.devtools = true;
+
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            titleMessage: 'Ciao, io sono scritto con Vue!'
+        }
+    }
+);
+
+
+
